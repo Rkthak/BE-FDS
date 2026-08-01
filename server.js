ff7760aca539ed75d9bda60c8ccde7d3fe165a64
Rkthak <rkon2609@gmail.com>
2026-08-01 13:12:29 +0530

    database connection --done

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require("express");
+const mongoose = require("mongoose");
+const { MONGODB_URI } = require("./utils/config");
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log("db connection error", err.message);
+  });
