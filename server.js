4fb2e5b2b3f3a0b5a5f3810762de156437b5a060
Rkthak <rkon2609@gmail.com>
2026-08-01 13:41:54 +0530

    app setup --done

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
-const { MONGODB_URI } = require("./utils/config");
+const { MONGODB_URI, PORT, HOST } = require("./utils/config");
+const app = require("./app");
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log("mongodb connected");
+
+    app
+      .listen(PORT, HOST, () => {
+        console.log("server listening ...");
+      })
+      .on("error", (err) => {
+        console.log("Error starting the server", err.message);
+      });
   })
   .catch((err) => {
     console.log("db connection error", err.message);
